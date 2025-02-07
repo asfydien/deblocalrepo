a0015e9d5fd0ab748fa504c758979a2029e98be7
Suhaya Komarudin <sofyan.wahyudin@gmail.com>
2025-02-07 17:55:10 +0700

    Bismillah...

--- lanrepo.sh
+++ lanrepo.sh
@@ -13,21 +13,25 @@ if [[ -z "$REPO_IP" ]]; then
     REPO_IP=$(hostname -I | awk '{print $1}')
 fi
 
+echo ""
 echo -e "\033[0;34m[Task]\033[0m Memastikan Apache dan alat pendukung terinstal..."
 apt update && apt install -y apache2 apt-mirror dpkg-dev
 
 REPO_DIR="/var/www/html/repo"
 CACHE_DIR="/repo-cache"
 
+echo ""
 echo -e "\033[0;34m[Task]\033[0m Menghapus paket lama di repository..."
 rm -rf "$REPO_DIR"
 mkdir -p "$REPO_DIR"
 chmod -R 755 "$REPO_DIR"
 
+echo ""
 echo -e "\033[0;34m[Task]\033[0m Menghapus cache lama dan menyiapkan direktori baru..."
 rm -rf "$CACHE_DIR"
 mkdir -p "$CACHE_DIR"
 
+echo ""
 echo -e "\033[0;34m[Task]\033[0m Mengunduh paket-paket LAMP dan dependensinya..."
 cd "$CACHE_DIR"
 
@@ -37,14 +41,17 @@ PACKAGES="apache2 mariadb-server php php-mysql libapache2-mod-php php-cli php-cu
 # Mengunduh semua paket beserta dependensinya
 apt download $PACKAGES
 
+echo ""
 echo -e "\033[0;34m[Task]\033[0m Memindahkan paket terbaru ke repository..."
 mv *.deb "$REPO_DIR/"
 
+echo ""
 echo "\033[0;34m[Task]\033[0m Membuat ulang indeks repository..."
 cd "$REPO_DIR"
 rm -f Packages.gz
 dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 
+echo ""
 echo -e "\033[0;34m[Task]\033[0m Memberikan izin yang sesuai..."
 chown -R www-data:www-data "$REPO_DIR"
 chmod -R 755 "$REPO_DIR"
@@ -54,7 +61,7 @@ echo "Repository lokal berhasil diperbarui!"
 echo "Repository tersedia di: http://$REPO_IP/repo"
 echo ""
 echo "Untuk menggunakan repository ini di client, tambahkan baris berikut ke /etc/apt/sources.list:"
-echo -e "\033[0;32m[Task]\033[0m deb [trusted=yes] http://$REPO_IP/repo ./"
+echo -e "\033[0;32mdeb [trusted=yes] http://$REPO_IP/repo ./"
 echo ""
 echo "Kemudian jalankan: apt update"
 echo ""
