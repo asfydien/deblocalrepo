f7e4e81053c13fba9331ac3f986ac491072d6845
Suhaya Komarudin <sofyan.wahyudin@gmail.com>
2025-02-08 13:20:48 +0700

    Update lanrepo.sh

--- lanrepo.sh
+++ lanrepo.sh
@@ -1,65 +1,110 @@
 #!/bin/bash
+#Daftar aplikasi yang digunakan untuk praktik latihan Debian 12
 
-# Pastikan script dijalankan sebagai root
-if [[ $EUID -ne 0 ]]; then
-    echo -e "\033[0;31m[Task]\033[0m Script ini harus dijalankan sebagai root!"
-    exit 1
-fi
+# Mengaktifkan opsi untuk menyimpan paket yang sudah diunduh
+echo -e "\033[0;34m[Task]\033[0m Persiapan..." 
+echo 'APT::Keep-Downloaded-Packages "true";' > /etc/apt/apt.conf.d/01keep-cache
 
-# Tentukan alamat IP repository (ubah sesuai kebutuhan)
-#REPO_IP="192.168.1.100"  # <-- Ganti dengan IP statis server Anda
-# Jika tidak ingin IP statis, gunakan deteksi otomatis:
-if [[ -z "$REPO_IP" ]]; then
-    REPO_IP=$(hostname -I | awk '{print $1}')
-fi
+# Lakukan update, dan download semua update-an
+echo -e "\033[0;34m[Task]\033[0m Update sistem..." 
+apt update -y && upgrade -y
+apt upgrade --download-only
 
-echo ""
-echo -e "\033[0;34m[Task]\033[0m Memastikan Apache dan alat pendukung terinstal..."
-apt update && apt install -y apache2 apt-mirror dpkg-dev
+echo -e "\033[0;34m[Task]\033[0m aptitude..." 
+apt install -y aptitude
 
-REPO_DIR="/var/www/html/repo"
-CACHE_DIR="/var/cache/apt/archives"
+echo -e "\033[0;34m[Task]\033[0m Downlaoad paket yang dibutuhkan..." 
+# Paket dhcp server
+aptitude install -d -y isc-dhcp-server
 
-echo ""
-echo -e "\033[0;34m[Task]\033[0m Membuat direktori repository jika belum ada..."
-mkdir -p "$REPO_DIR"
-chmod -R 755 "$REPO_DIR"
+# Paket FTP Server
+aptitude install -d -y proftpd 
+aptitude install -d -y vsftpd 
+aptitude install -d -y pure-ftpd
 
-echo ""
-echo -e "\033[0;34m[Task]\033[0m Memastikan direktori cache tersedia..."
-mkdir -p "$CACHE_DIR/partial"
+# Paket Remote Server
+aptitude install -d -y freeradius freeradius-mysql freeradius-utils
+aptitude install -d -y openssh-server openssh-client openssh-sftp-server sshpass ntp
 
-echo ""
-echo -e "\033[0;34m[Task]\033[0m Mengunduh paket-paket LAMP dan dependensinya..."
-# Daftar paket LAMP
-PACKAGES="apache2 mariadb-server php php-mysql libapache2-mod-php php-cli php-curl php-gd php-mbstring php-xml php-zip"
+# Paket File Server
+aptitude install -d -y samba smbclient
+aptitude install -d -y winbind libpam-winbind libnss-winbind krb5-config samba-dsdb-modules samba-vfs-modules
+aptitude install -d -y slapd ldap-utils
 
-# Unduh ulang paket, meskipun sudah terinstal
-apt update
-apt install -d --download -y $PACKAGES
+# Paket Web Server
+aptitude install -d -y apache2 php
+aptitude install -d -y perl ruby openssl python3 certbot apache2-utils
+aptitude install -d -y nginx php-fpm nginx-extras libpam-krb5
+aptitude install -d -y lighttpd
+aptitude install -d -y php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip php-pgsql php-mysql
+aptitude install -d -y php-common php-enchant hunspell
 
-echo ""
-echo -e "\033[0;34m[Task]\033[0m Memindahkan paket terbaru ke repository..."
-mv "$CACHE_DIR"/*.deb "$REPO_DIR/"
+# Paket DNS Server
+aptitude install -d -y bind9 bind9utils dnsutils
+aptitude install -d -y dnsmasq resolvconf 
 
-echo ""
-echo -e "\033[0;34m[Task]\033[0m Membuat ulang indeks repository..."
-cd "$REPO_DIR"
-rm -f Packages.gz
-dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+#Paket Database Server
+aptitude install -d -y mariadb-server mariadb-client phpmyadmin
+aptitude install -d -y postgresql
 
-echo ""
-echo -e "\033[0;34m[Task]\033[0m Memberikan izin yang sesuai..."
-chown -R www-data:www-data "$REPO_DIR"
-chmod -R 755 "$REPO_DIR"
+# Paket Mail Server
+aptitude install -d -y postfix sasl2-bin
+aptitude install -d -y dovecot-core dovecot-pop3d dovecot-imapd
+aptitude install -d -y roundcube courier-imap courier-pop
+aptitude install -d -y clamav-daemon amavisd-new
 
-echo ""
-echo -e "\033[0;34m[Info]\033[0m Repository lokal berhasil dibuat!"
-echo "Repository tersedia di: http://$REPO_IP/repo"
-echo ""
-echo "Untuk menggunakan repository ini di client, tambahkan baris berikut ke /etc/apt/sources.list:"
-echo ""
-echo -e "\033[0;32mdeb [trusted=yes] http://$REPO_IP/repo ./\033[0m"
-echo ""
-echo "Kemudian jalankan: apt update"
-echo ""
+# Paket Control Panel
+aptitude install -d -y cockpit
+aptitude install -d -y libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions
+aptitude install -d -y shared-mime-info unzip wget curl
+
+# Paket VPN
+aptitude install -d -y pptpd openvpn
+aptitude install -d -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison gcc make libnss3-tools libevent-dev libsystemd-dev uuid-runtime ppp xl2tpd
+aptitude install -d -y fail2ban
+
+# Paket Monitoring
+aptitude install -d -y netdata sysstat
+aptitude install -d -y cacti snmpd
+aptitude install -d -y htop
+
+# Paket Extra
+aptitude install -d -y net-tools iptables iproute2 gawk grep sed net-tools
+aptitude install -d -y dpkg-dev
+aptitude install -d -y squid
+#aptitude install -d -y asterisk
+aptitude install -d -y zip
+aptitude install -d -y ufw
+
+echo -e "\033[0;34m[Task]\033[0m Persiapan membuat repository..." 
+# Instal semua alat yang dibutuhkan
+apt install apache2 dpkg-dev
+
+echo -e "\033[0;34m[Task]\033[0m Menyalin semua cache..." 
+mkdir -p /var/www/html/repo
+cd /var/cache/apt/archives
+
+# bersihkan dari karakter % atau %253a jadi -
+for file in *; do
+    new_name=$(echo "$file" | sed 's/%253a/-/g; s/%/-/g')
+    cp "$file" "/var/www/html/repo/$new_name"
+done
+
+echo -e "\033[0;34m[Task]\033[0m Menyusun index..." 
+cd /var/www/html/repo
+dpkg-scanpackages -m . > Packages
+dpkg-scanpackages -m . | gzip > Packages.gz
+
+# Berikan izin yang sesuai pada direktori repository
+chown -R www-data:www-data /var/www/html/repo
+chmod -R 755 /var/www/html/repo
+
+# Dapatkan alamat IP dari CT Debian
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
+
+echo -e "\033[0;34m[Task]\033[0m Selesai..." 
+echo -e ""
+echo -e "Tambahkan baris berikut ke /etc/apt/sources.list"
+echo -e ""
+echo -e "   deb [trusted=yes] http://$IP_ADDRESS/local-repo/ ./"
+echo -e ""
